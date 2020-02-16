255f6389c170da9fcaf6245ae65524bc14bee1c3
Tyler Lai <lai.t.tyler@gmail.com>
2020-02-16 07:30:43 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,40 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  
+  dwarves.each_with_index { |item, index | puts "#{index + 1}: #{item}" }  
+  
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(calls)
+  result = []
+  result = calls.collect { |c| c.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
+# def long_planeteer_calls(calls)
+   
+#   if calls.length <= 3
+#     false
+#   else
+#     true
+#   end 
+# end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(food)
+  
   cheese_types = ["cheddar", "gouda", "camembert"]
+  i = 0
+  while i < cheese_types.length do
+    
+    if food.include?(cheese_types[i])
+      
+      return cheese_types[i]
+
+    else
+      
+      nil
+
+    end
+
+    i += 1
+  end 
+  
 end
